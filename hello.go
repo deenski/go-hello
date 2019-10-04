6afadfd5b5c75ba84b4a0a6ee946516a6dbd8beb
Jakob Vendegna <jakob.vendegna@dotedu.tech>
2019-10-04 17:08:19 -0600

    used stringutil.Reverse package to do some string reversing

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/deenski/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world. @Deenski be doing go code.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
