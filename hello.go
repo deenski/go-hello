52846ab2b5839bf875025cc4e684600de1ac64c1
Jakob Vendegna <jakob.vendegna@dotedu.tech>
2019-10-05 12:00:25 -0600

    updated hello with new functions from stringutil package

--- hello.go
+++ hello.go
@@ -8,4 +8,17 @@ import (
 
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+
+	fmt.Println(stringutil.ToLower("ALL OF THESE LETTERS ARE LOWER CASE"))
+
+	fmt.Println(stringutil.ToUpper("and this string is upper case"))
+
+	fmt.Print(stringutil.CompareThese("four", "five"))
+	fmt.Print(stringutil.CompareThese("two", "three"))
+	fmt.Print(stringutil.CompareThese("nineteen", "one"))
+
+	a := "This is a "
+	b := "concatinated string."
+
+	fmt.Println(stringutil.ConcatThese(a, b))
 }
